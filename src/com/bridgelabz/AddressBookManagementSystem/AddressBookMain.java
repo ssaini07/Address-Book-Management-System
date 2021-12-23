372067d6d5e20bfa0b08fc252ed99b5a63ff7b34
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-23 14:27:08 +0530

    Displaying welcome to the address book management program

---
+++ src/com/bridgelabz/AddressBookManagementSystem/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.AddressBookManagementSystem;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the address book program");
+
+	}
+
+}
